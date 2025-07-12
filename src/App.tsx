b4e0f0f56b49f53b8a54a9387628b7b4d2227e50
Dayane Rodrigues <dayane.rodrigues@softplan.com.br>
2025-07-12 14:18:12 -0300

    Text and Icon component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
-export default function App() {
+import Text from "./components/Text";
+import TrashIcon from "./assets/icons/trash.svg?react";
+import Icon from "./components/Icon";
 
-  return (
-    <div className="text-pink-base">
-      Olá mundo
-    </div>
-  )
+export default function App() {
+	return (
+		<div className="grid gap-3">
+			<div className="flex flex-col gap-1">
+				<Text variant="body-sm-bold" className="text-pink-base">
+					Olá mundo
+				</Text>
+				<Text className="text-green-base">Texto 2</Text>
+				<Text variant="body-md-bold" className="text-gray-400">
+					Texto 3
+				</Text>
+			</div>
+
+			<div className="flex gap-1">
+				<Icon svg={TrashIcon} className="fill-pink-base" />
+				<Icon svg={TrashIcon} className="fill-pink-base" />
+			</div>
+		</div>
+	);
 }
