e112b5c626abc0dd2c5a6ffaee4c7969d77ab499
Dayane Rodrigues <dayane.rodrigues@softplan.com.br>
2025-07-12 14:46:46 -0300

    Badge

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Text from "./components/Text";
 import TrashIcon from "./assets/icons/trash.svg?react";
 import Icon from "./components/Icon";
+import Badge from "./components/Badge";
 
 export default function App() {
 	return (
@@ -18,6 +19,8 @@ export default function App() {
 			<div className="flex gap-1">
 				<Icon svg={TrashIcon} className="fill-pink-base" />
 				<Icon svg={TrashIcon} className="fill-pink-base" />
+				<Badge variant="primary">5</Badge>
+				<Badge variant="secondary">8234</Badge>
 			</div>
 		</div>
 	);
